323cfb997eb0078db199152342462503a30f567b
David_Holliday <dhholliday@icloud.com>
2023-05-21 14:59:24 +0100

    correct netlify typo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ export default function Contact() {
     <section id="contact" className="relative">
       <div className="container px5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <form
-          netflify
+          netlify
           name="contact"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:mx-auto w-full md:py-8 mt-8 md:mt-0 mx-10"
         >
